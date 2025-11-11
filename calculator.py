49a12c79f0bae79038b79e3e8dc2c0b21ccfdc33
pinkytoefoo <agadrian1331@gmail.com>
2025-11-11 16:19:59 -0500

    modified calculator p1

--- calculator.py
+++ calculator.py
@@ -7,24 +7,29 @@ One function per operation, in order.
 
 import math
 
-# First example
-def add(a, b): 
-    return a+b
+def add(a, b):
+    return a + b
+
 def sub(a, b):
-    return a-b
+    return a - b
+
 def mul(a, b):
-    return a*b
+    return a * b
+
 def div(a, b):
     if a == 0:
-        raise ZeroDivisionError
-    return a/b
-def log1(a, b):
-    if(a > 0 and b > 0 and a!=1):
-        return math.log(a,b)
+        raise ZeroDivisionError()
+    return b / a
+
+def log(a, b):
+    if a > 0 and b > 0 and a!=1:
+        math.log(b, a)# use math library + raise ValueError
     else:
         raise ValueError
+
 def exp(a, b):
-    return a**b
+    return a^b
+
 
 
 
